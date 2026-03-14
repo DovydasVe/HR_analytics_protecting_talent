9207ba3aa3aa75cdda9ec8a7ba8a67f236eac051
Dovydas <dovydas.venc@gmail.com>
2026-03-14 17:16:22 +0100

    fixed talent formula cutoff + graph

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,13 +1,9 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-# Load data
 df = pd.read_excel("ABS Tech Case 2026_Data.xlsx")
-
-# Capitalize entries in column for hispanic/latino
 df["HispanicLatino"] = df["HispanicLatino"].str.capitalize()
 
-# Columns to scale
 cols_to_scale = [
     "EngagementSurvey", "EmpSatisfaction", "SpecialProjectsCount",
     "DaysLateLast30", "Absences", "ManPos", "TechLev", "JobStr",
@@ -17,15 +13,11 @@ cols_to_scale = [
     "PerfScore"
 ]
 
-# Convert selected columns to numeric (in case Excel stored them as text)
 df[cols_to_scale] = df[cols_to_scale].apply(pd.to_numeric, errors="coerce")
 
-# Standardize only the selected columns
 scaler = StandardScaler()
 df_scaled = scaler.fit_transform(df[cols_to_scale])
 
-# Put scaled values back into the original dataframe
 df[cols_to_scale] = df_scaled
 
-# Save full dataset with scaled columns
 df.to_excel("Cleaned_ABS_Tech_Case_2026_Data.xlsx", index=False)
